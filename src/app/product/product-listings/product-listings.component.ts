a4729b6ff6c948b62ed19e73638944e06a3cf3bf
yhayami <>
2022-09-18 15:00:25 +0900

    product-list から product-listings に名称変更

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products = [1, 2, 3, 4];
