99a927e79aaac8b7a931eb067e4ddeeb835bea27
yhayami <>
2022-09-18 15:41:37 +0900

    ルートパラメータを使って詳細画面を表示

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/app/products';
+
+type Product = typeof products[0];
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const productId = parseInt(params.get('productId')!);
+      this.product = products[productId]
+    })
   }
 
 }
