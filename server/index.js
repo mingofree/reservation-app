b537420ff741af53359a2f7df37b06d1df55bf28
yhayami <>
2022-09-18 22:46:18 +0900

    エンドポイントの動作チェック

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || '3001'
+app.get("/products", function (req, res) {
+  res.json({ 'success': true });
+});
 
-app.listen(PORT, function() {
-  console.log('I am running!')
-})
+const PORT = process.env.PORT || "3001";
+
+app.listen(PORT, function () {
+  console.log("I am running!:" + PORT);
+});
