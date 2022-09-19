661edbd7d29d72a37f136885d90e8cc5763a9d3e
yhayami <>
2022-09-19 13:15:08 +0900

    findById 追加

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const Product = require('../model/product')
+const Product = require("../model/product");
 
 router.get("", function (req, res) {
   Product.find({}, function (err, foundProducts) {
     return res.json(foundProducts);
+  });
+});
+
+router.get("/:productId", function (req, res) {
+  const productId = req.params.productId;
+  // console.log(`productId = ${productId}`);
+
+  Product.findById(productId, function (err, foundProduct) {
+    if (err) {
+      return res
+        .status(422)
+        .send({
+          errors: [{ title: "Product error", detail: "Product not found!" }],
+        });
+    }
+    console.log(foundProduct);
+    return res.json(foundProduct);
   });
 });
 
