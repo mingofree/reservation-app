5ebbbcc6f0dff5d8aadc5e6081b06660549ce15c
yhayami <>
2022-09-19 12:25:46 +0900

    MongoDB に接続

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+try {
+  mongoose.connect(
+    "mongodb+srv://mingo:AheAheUhiha123@cluster0.qyc0y.mongodb.net/appDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  console.log("Success: Connected to MongoDB");
+} catch (err) {
+  console.log(`Erro: ${err.message}`);
+  throw new Error(err.message);
+}
 
 const app = express();
 
 app.get("/products", function (req, res) {
-  res.json({ 'success': true });
+  res.json({ success: true });
 });
 
 const PORT = process.env.PORT || "3001";
