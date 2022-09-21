6086de28b7167067b32bf5794c6691e2a3340fb5
yhayami <>
2022-09-21 15:05:12 +0900

    ログイン＆新規会員登録(バックエンド)：DBにユーザー登録

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,12 +6,102 @@ const User = require("../model/user");
 
 router.post("/login", function (req, res) {
   const { email, password } = req.body;
-  return res.json({ email, password });
+
+  if (!email) {
+    return res.status(422).send({
+      errors: [{ title: "User error", detail: "Please fill email!" }],
+    });
+  }
+  if (!password) {
+    return res.status(422).send({
+      errors: [{ title: "User error", detail: "Please fill password!" }],
+    });
+  }
+
+  User.findOne({ email }, function (err, foundUser) {
+    if (err) {
+      return res.status(422).send({
+        errors: [{ title: "User error", detail: "Someting went wrong!" }],
+      });
+    }
+    if (!foundUser) {
+      return res.status(422).send({
+        errors: [{ title: "User error", detail: "User is not exist!" }],
+      });
+    }
+    if (!foundUser.hasSamePassword(password)) {
+      return res.status(422).send({
+        errors: [{ title: "User error", detail: "Incorrect password!" }],
+      });
+    }
+
+    // const token = jwt.sign(
+    //   {
+    //     userId: foundUser.id,
+    //     username: foundUser.username,
+    //   },
+    //   config.SECRET,
+    //   { expiresIn: "1h" }
+    // );
+
+    return res.json(token);
+  });
 });
 
 router.post("/register", function (req, res) {
   const { username, email, password, confirmPassword } = req.body;
-  return res.json({ username, email, password, confirmPassword });
+
+  if (!username) {
+    return res.status(422).send({
+      errors: [
+        { title: "User error", detail: "ユーザー名を入力してください！" },
+      ],
+    });
+  }
+  if (!email) {
+    return res.status(422).send({
+      errors: [{ title: "User error", detail: "Please fill email!" }],
+    });
+  }
+  if (!password) {
+    return res.status(422).send({
+      errors: [{ title: "User error", detail: "Please fill password!" }],
+    });
+  }
+  if (password !== confirmPassword) {
+    return res.status(422).send({
+      errors: [{ title: "User error", detail: "Please check passwords!" }],
+    });
+  }
+
+  User.findOne({ email }, function (err, foundUser) {
+    if (err) {
+      return res
+        .status(422)
+        .send({
+          errors: [{ title: "User error", detail: "Someting went wrong!" }],
+        });
+    }
+    if (foundUser) {
+      return res
+        .status(422)
+        .send({
+          errors: [{ title: "User error", detail: "User already exist!" }],
+        });
+    }
+
+    const user = new User({ username, email, password });
+    user.save(function (err) {
+      if (err) {
+        return res
+          .status(422)
+          .send({
+            errors: [{ title: "User error", detail: "Someting went wrong!" }],
+          });
+      }
+      return res.json({ registerd: true });
+    });
+  });
 });
 
 module.exports = router;
