39adf6d49cdf2ec80181de00a6cb68adeed6954d
yhayami <>
2022-09-21 11:17:46 +0900

    フロントエンドデザインの作り込み：第１段階

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  title = 'sample-app';
+export class NavbarComponent implements OnInit {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+
+  constructor(
+    public location: Location,
+    private element: ElementRef // public auth: AuthService
+  ) {
+    this.sidebarVisible = false;
+  }
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+  }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  }
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  }
+  sidebarToggle() {
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  }
 }
